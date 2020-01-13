ce994c38c56e3144b16704dd4204f9386b466185
MrOffer <thomas.e.offer@gmail.com>
2020-01-13 16:57:54 -0600

    started exercise

---
+++ src/main/java/tableMigration.sql
@@ -0,0 +1,19 @@
+use adlister_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL ,
+    email  VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE IF NOT EXISTS ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED DEFAULT NULL,
+    title  VARCHAR(100) NOT NULL,
+    description text(500) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
