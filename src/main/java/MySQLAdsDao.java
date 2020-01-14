b26991277146f99c9d687b74d623754f7d0c9428
MrOffer <thomas.e.offer@gmail.com>
2020-01-14 12:14:03 -0600

    maybe done?

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -5,6 +5,7 @@ import com.mysql.cj.jdbc.Driver;
 public class MySQLAdsDao implements Ads {
 
     private Connection connection;
+
     public MySQLAdsDao() {
         try {
             DriverManager.registerDriver(new Driver());
@@ -18,38 +19,44 @@ public class MySQLAdsDao implements Ads {
         }
 
     }
+
     @Override
     public List<Ad> all() {
-            List<Ad> output = new ArrayList<>();
+        List<Ad> output = new ArrayList<>();
 
-            String query = "Select * FROM ads";
+        String query = "Select * FROM ads";
 
-            try {
-                Statement statement = connection.createStatement();
-                ResultSet rs = statement.executeQuery(query);
-                while(rs.next()) {
-                    output.add(
-                            new Ad(
-                                    rs.getLong("id"),
-                                    rs.getLong("userId"),
-                                    rs.getString("title"),
-                                    rs.getString("description")
-                            )
-                    );
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                output.add(
+                        new Ad(
+                                rs.getLong("id"),
+                                rs.getLong("userId"),
+                                rs.getString("title"),
+                                rs.getString("description")
+                        )
+                );
             }
-
-
-            return output;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
 
+        return output;
+    }
 
 
     @Override
-    public Long insert(Ad ad) {
-        return null;
+    public void insert(Ad ad) {
+        try {
+            Statement statement = connection.createStatement();
+            String insertQuery = String.format("INSERT INTO ads (title, description) VALUES ('%s', '%s')");
+            statement.executeUpdate(insertQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
+
